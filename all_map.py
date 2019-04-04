6916cbd1231d03cb22e67bc72838841e99a67fd7
LU-K-Brant <42348398+LU-K-Brant@users.noreply.github.com>
2019-04-04 21:39:50 +0800

    map计算脚本

---
+++ all_map.py
@@ -0,0 +1,12 @@
+from voc_eval import voc_eval
+import os
+FindPath = '/home/lzm/data/darknet/results/' #valid生成的测试结果文件路径
+FileNames = os.listdir(FindPath)
+#for file_name in FileNames:
+for file in FileNames:
+    file_name = file.split('.')[0]
+    print voc_eval('/home/lzm/data/darknet/results/{}.txt',  #valid生成的测试结果文件
+              '/home/lzm/data/darknet/gpu_train/xml/{}.xml', #标签路径
+              '/home/lzm/data/darknet/test.txt', #只需要train.txt里面的图片名，不带路径和后缀
+               file_name, '.')
+
