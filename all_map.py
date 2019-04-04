2cdfd5209e91e2351113eb722b0c901d101200c2
LU-K-Brant <42348398+LU-K-Brant@users.noreply.github.com>
2019-04-04 21:42:29 +0800

    支持python3

--- all_map.py
+++ all_map.py
@@ -5,8 +5,8 @@ FileNames = os.listdir(FindPath)
 #for file_name in FileNames:
 for file in FileNames:
     file_name = file.split('.')[0]
-    print voc_eval('/home/lzm/data/darknet/results/{}.txt',  #valid生成的测试结果文件
+    print(voc_eval('/home/lzm/data/darknet/results/{}.txt',  #valid生成的测试结果文件
               '/home/lzm/data/darknet/gpu_train/xml/{}.xml', #标签路径
               '/home/lzm/data/darknet/test.txt', #只需要train.txt里面的图片名，不带路径和后缀
-               file_name, '.')
+               file_name, '.'))
 
